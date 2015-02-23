b4089763b8136831c8b8dc40227279e3d90bc0b8
Alex Greene <alexgrn7@gmail.com>
2015-02-23 14:41:41 -0800

    Tap the screen to alternate between blue/yellow for all connected. This is the foundation upon which the game will be built.

--- ColorsGame/ViewController.swift
+++ ColorsGame/ViewController.swift
@@ -7,19 +7,127 @@
 //
 
 import UIKit
+import MultipeerConnectivity
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, MCBrowserViewControllerDelegate,
+MCSessionDelegate {
+    
+    let serviceType = "LCOC-Chat"
+    
+    var browser : MCBrowserViewController!
+    var assistant : MCAdvertiserAssistant!
+    var session : MCSession!
+    var peerID: MCPeerID!
+    
+    
+    @IBOutlet var colorView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        self.peerID = MCPeerID(displayName: UIDevice.currentDevice().name)
+        self.session = MCSession(peer: peerID)
+        self.session.delegate = self
+        
+        self.browser = MCBrowserViewController(serviceType:serviceType,
+            session:self.session)
+        
+        self.browser.delegate = self;
+        
+        self.assistant = MCAdvertiserAssistant(serviceType:serviceType,
+            discoveryInfo:nil, session:self.session)
+        
+        self.assistant.start()
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    func updateColor(isYellow : Int, fromPeer peerID: MCPeerID) {
+        
+        if isYellow == 1 {
+            self.colorView.backgroundColor = UIColor.blueColor()
+        }
+        else {
+            self.colorView.backgroundColor = UIColor.yellowColor()
+        }
+        
     }
-
-
+    
+    
+    @IBAction func sendColor(sender: AnyObject) {
+        
+        var error : NSError?
+        
+        var isYellow: Int
+        
+        if self.colorView.backgroundColor == UIColor.yellowColor() {
+            isYellow = 1
+        }
+        else {
+            isYellow = 0
+        }
+        
+        self.session.sendData(NSData(bytes:&isYellow, length: 1), toPeers: self.session.connectedPeers,
+            withMode: MCSessionSendDataMode.Unreliable, error: &error)
+        
+        if error != nil {
+            print("Error sending data: \(error?.localizedDescription)")
+        }
+        
+        self.updateColor(isYellow, fromPeer: self.peerID)
+    }
+    
+    
+    @IBAction func openBrowser(sender: AnyObject) {
+        
+        self.presentViewController(self.browser, animated: true, completion: nil)
+    }
+    
+    
+    func browserViewControllerDidFinish(
+        browserViewController: MCBrowserViewController!)  {
+            
+            self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func browserViewControllerWasCancelled(
+        browserViewController: MCBrowserViewController!)  {
+            
+            self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func session(session: MCSession!, didReceiveData data: NSData!,
+        fromPeer peerID: MCPeerID!)  {
+            
+            var temp: Int = 0
+            data.getBytes(&temp, length: 1)
+            dispatch_async(dispatch_get_main_queue()) {
+                
+                self.updateColor(temp, fromPeer: peerID)
+            }
+    }
+    
+    func session(session: MCSession!,
+        didStartReceivingResourceWithName resourceName: String!,
+        fromPeer peerID: MCPeerID!, withProgress progress: NSProgress!)  {
+    }
+    
+    func session(session: MCSession!,
+        didFinishReceivingResourceWithName resourceName: String!,
+        fromPeer peerID: MCPeerID!,
+        atURL localURL: NSURL!, withError error: NSError!)  {
+    }
+    
+    func session(session: MCSession!, didReceiveStream stream: NSInputStream!,
+        withName streamName: String!, fromPeer peerID: MCPeerID!)  {
+    }
+    
+    func session(session: MCSession!, peer peerID: MCPeerID!,
+        didChangeState state: MCSessionState)  {
+            
+    }
+    
+    
+    
 }
-
